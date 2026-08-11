673a10b7b1a3393db0e02fa7ed9dcfe2493425e4
Gopal Gandhi <gandhigl@rknec.edu>
2026-08-11 23:38:47 +0530

    changes for deployement

--- selfBackend/server.js
+++ selfBackend/server.js
@@ -29,6 +29,8 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.log(err));
 
 // server
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
